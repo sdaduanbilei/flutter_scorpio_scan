ee8188f76b2b33f4ebdad95807e617353af57cc8
sdaduanbilei <sdaduanbilei@163.com>
2020-12-07 17:32:36 +0800

    [add]Android 扫描界面

--- android/src/main/kotlin/com/scopro/flutter_plugin_scan/FlutterPluginScanPlugin.kt
+++ android/src/main/kotlin/com/scopro/flutter_plugin_scan/FlutterPluginScanPlugin.kt
@@ -1,5 +1,6 @@
 package com.scopro.flutter_plugin_scan
 
+import android.content.Context
 import android.content.Intent
 import androidx.annotation.NonNull
 
@@ -17,15 +18,19 @@ class FlutterPluginScanPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var context : Context
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_plugin_scan")
+    context = flutterPluginBinding.applicationContext
     channel.setMethodCallHandler(this)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "startScan") {
-
+      val intent = Intent(context,ScanActivity::class.java)
+      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+      context.startActivity(intent)
       result.success("Android ${android.os.Build.BOARD}")
     } else {
       result.notImplemented()
--- android/src/main/kotlin/com/scopro/flutter_plugin_scan/ScanActivity.kt
+++ android/src/main/kotlin/com/scopro/flutter_plugin_scan/ScanActivity.kt
@@ -1,11 +1,24 @@
 package com.scopro.flutter_plugin_scan
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.ResultPoint
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.BarcodeView
+
 
 class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+        findViewById<Button>(R.id.btnScan).setOnClickListener {
+            IntentIntegrator(this)
+                    .setPrompt("扫二维码 / 条码")
+                    .initiateScan()
+        }
     }
 }
